2297898ecc9175a6b8a956f796ec0e09709e4f71
xubenqing_123 <xubenqing@enjoyk.cn>
2020-08-28 18:52:49 +0800

    dao

---
+++ mybaits_2dao/src/com/atguigu/mybaits/bean/User.java
@@ -0,0 +1,34 @@
+package com.atguigu.mybaits.bean;
+
+public class User {
+
+	private int id;
+	private String lastName;
+	private int sex;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", lastName=" + lastName + ", sex=" + sex + "]";
+	}
+	
+	
+}
