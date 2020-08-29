da885ec492f00fd83649860fa829866d836fb085
xibanqiu <844040829@qq.com>
2020-08-29 17:45:15 +0800

    mapper crud

---
+++ mybaits_3mapper/src/com/atguigu/mybaits/test/UserMapperTest.java
@@ -0,0 +1,95 @@
+package com.atguigu.mybaits.test;
+
+import com.atguigu.mybaits.bean.User;
+import com.atguigu.mybaits.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+public class UserMapperTest {
+
+    static SqlSessionFactory sqlSessionFactory;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception{
+
+        String url = "mybaits-config.xml";
+        InputStream in = Resources.getResourceAsStream(url);
+
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+    }
+
+    @Test
+    public void testSaveUser(){
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        try {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            User user = new User(5,"huang",0);
+            mapper.saveUser(user);
+
+            sqlSession.commit();
+            System.out.println(user);
+
+        }finally {
+            sqlSession.close();
+        }
+
+    }
+
+    @Test
+    public void testUpdateUser() {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            User user = new User(5, "eeeee", 1);
+            userMapper.updateUser(user);
+            session.commit();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Test
+    public void testDeleteUserById() {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            userMapper.deleteUserById(5);
+            session.commit();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Test
+    public void testFindUserById() {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            System.out.println(userMapper.findUserById(1));
+        } finally {
+            session.close();
+        }
+    }
+
+    @Test
+    public void testFindUsers() {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            System.out.println(userMapper.findUsers());
+        } finally {
+            session.close();
+        }
+    }
+
+}
+
+
