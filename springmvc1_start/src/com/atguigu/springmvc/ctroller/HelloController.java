6955404977f0162aee3eb7d46449bc864fec82c1
xibanqiu <844040829@qq.com>
2020-12-10 15:08:36 +0800

    springStart commit

---
+++ springmvc1_start/src/com/atguigu/springmvc/ctroller/HelloController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springmvc.ctroller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+
+        System.out.println(111);
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("hello");
+        mv.addObject("msg","第一个spring 程序");
+
+        return mv;
+    }
+}
---
+++ springmvc1_start/web/WEB-INF/views/hello.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<div sytle = "color:red; font-size:30px"> ${msg}</div>
+
+</body>
+</html>
